501679cb4dadf925de834b1ba44eec5bfcaa3e1d
Daniel Germann <dge@bytemine.net>
2021-06-29 10:40:19 +0200

    added variables for better flow controll

--- src/restic_compose_backup/alerts/smtp.py
+++ src/restic_compose_backup/alerts/smtp.py
@@ -3,20 +3,23 @@ import smtplib
 import logging
 from email.mime.text import MIMEText
 
-from restic_compose_backup.alerts.base import BaseAlert
+# from restic_compose_backup.alerts.base import BaseAlert
 
 logger = logging.getLogger(__name__)
 
 
-class SMTPAlert(BaseAlert):
+class SMTPAlert():
     name = 'smtp'
 
-    def __init__(self, host, port, user, password, to):
+    def __init__(self, host, port, user, password, to, auth, ssl):
         self.host = host
         self.port = port
         self.user = user
-        self.password = password or ""
+        self.password = password
         self.to = to
+        self.auth = auth
+        self.ssl = ssl
+
 
     @classmethod
     def create_from_env(cls):
@@ -26,6 +29,8 @@ class SMTPAlert(BaseAlert):
             os.environ.get('EMAIL_HOST_USER'),
             os.environ.get('EMAIL_HOST_PASSWORD'),
             (os.environ.get('EMAIL_SEND_TO') or "").split(','),
+            os.environ.get('EMAIL_AUTH'),
+            os.environ.get('EMAIL_SSL'),
         )
         if instance.properly_configured:
             return instance
@@ -45,9 +50,13 @@ class SMTPAlert(BaseAlert):
 
         try:
             logger.info("Connecting to %s port %s", self.host, self.port)
-            server = smtplib.SMTP(self.host, self.port)
+            if self.ssl == "TRUE":
+                server = smtplib.SMTP_SSL(self.host, self.port)
+            else:
+                server = smtplib.SMTP(self.host, self.port)
+            if self.auth == "TRUE":
+                server.login(self.user, self.password)
             server.ehlo()
-            #server.login(self.user, self.password)
             server.sendmail(self.user, self.to, msg.as_string())
             logger.info('Email sent')
         except Exception as ex:
